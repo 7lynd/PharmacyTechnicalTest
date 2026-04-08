5c13357e7c81d72dd4ad7ed58ec41ba8ed200d1d
Cédric Dos Santos <cedric.dossantos@einie.fr>
2026-04-08 13:08:37 +0200

    refactor: pharmacy

--- pharmacy.js
+++ pharmacy.js
@@ -11,55 +11,46 @@ export class Pharmacy {
     this.drugs = drugs;
   }
   updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
+    // On va parcourir l'ensemble des drugs pour gerer les differents cas et decrementer le expiresIn
+    this.drugs.forEach(drug => {
+      // Si c'est Magic Pill on passe directement au suivant car celui-ci n'est pas affecté par quoi que ce soit. 
+      if (drug.name === "Magic Pill") return;
+      
+      if (drug.name === "Fervex") {
+        // Si c'est inferieur a 50 on va pouvoir proceder a une incrementation
+        if (drug.benefit < 50) {
+          if (drug.expiresIn <= 0) { 
+            drug.benefit = 0;
+          } else if (drug.expiresIn < 6) {
+            drug.benefit += 3;
+          } else if (drug.expiresIn < 11) {
+            drug.benefit += 2;
+          } else { 
+            drug.benefit +=1;
+          }
+        }
+      } else if (drug.name === "Herbal Tea") {
+        // Si c'est inferieur a 50 on va pouvoir proceder a une incrementation
+        if (drug.benefit < 50) {
+          if (drug.expiresIn > 0) {
+            drug.benefit += 1;
+          } else {
+            drug.benefit += 2;
           }
         }
       } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
+        // Pour le cas d'un medicament normal, on applique une strategie de decrementation mais que si benefit est superieur a 0
+        if (drug.benefit > 0) {
+          if (drug.expiresIn > 0) { 
+            drug.benefit -= 1;
           } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+            drug.benefit -= 2;
           }
         }
       }
-    }
+
+      drug.expiresIn -= 1;
+    })
 
     return this.drugs;
   }
